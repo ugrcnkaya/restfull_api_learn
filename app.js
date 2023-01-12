ee5ad42331cdf3ead91fedca10fa111f3f66a2a8
Ugurcan Kaya <118749864+ugrcnkaya@users.noreply.github.com>
2023-01-12 23:09:05 +0100

    Initial

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
 const express = require('express');
-
+const dbPassword = require('./local')
 const app = express();
+
+
+
+
+
+//1-Middlewares
+
+//function that executes when specific routes are being hit. (bus)
+/*
+* For example = auth can be a middleware feature.
+* */
+app.use('/posts', () => {
+   // console.log("this is a middleware running.")
+});
+
+//2-Routes
+//get, post, delete, patch
+app.get('/', (req, res) => {
+    res.send('We are on home.');
+});
+
+app.get('/posts', (req, res) => {
+    res.send('We are on posts.');
+});
+
+//3-How do we start listening to the server?
+app.listen(3000);
+
